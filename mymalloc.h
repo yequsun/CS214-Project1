d446b7bd658f863e391c5aef48673943e5daa39e
Yequ Sun <yequsun@gmail.com>
2016-10-06 13:12:54 -0400

    New Metadata Struct
    Finally I came up with the best design for the metadata so far. They are
    a little different from the one we discussed before.
    
    typedef struct{
    unsigned int size:31;
    unsigned int alloc_flag:1;
    unsigned int prev_index:31;
    unsigned int last_flag:1;
    }Metadata
    
    In the previous design, the length of prev_index and next_index are the
    bottle neck for this implementation. Even if size can hold up to 31
    bits, prev and next references can only hold 16 bits.
    This new design solves the problem. It stores prev index and a flag to
    tell if this block is the last one.
    Two utility functions are used to return the prev and next pointer.
    Prev pointer is directly get from the index.
    Next pointer is calculated with the current pointer and size.
    last_flag is very useful for traversing the list.
    
    I also wrote two utility functions to get the pointers of prev and next
    block. They'll be included in mymalloc.c later.

--- mymalloc.h
+++ mymalloc.h
@@ -4,10 +4,24 @@
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
 
+#include <stdlib.h>
+
 static char myblock[5000];
 
-void* mymalloc(size_t size);
+typedef struct{
+	unsigned int size:31;//The size of this block
+	unsigned int alloc_flag:1;//If the block is allocated. 0 - not. 1 - yes
+	unsigned int prev_index:31;//The index number in myblock[] of the previous block metadata
+	unsigned int last_flag:1;//If this block is the last block. 0- not. 1 -yes
+}Metadata;
 
-void myfree(void* ptr);
+Metadata* prev(Metadata*); //returns the pointer to the previous block.
+
+Metadata* next(Metadata*); //returns the pointer to the next block.
+
+
+void* mymalloc(size_t size, const char* file_name, int line_number);
+
+void myfree(void* ptr, const char* file_name, int line_number);
 
 #endif
